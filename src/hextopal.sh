534893a4b24cd22f581745b8112c825969e4c31f
Jordanyay <99468612+Jordanyay@users.noreply.github.com>
2023-01-24 23:02:22 +1300

    Update files via upload
    Add experimental "object" tests.

--- src/hextopal.sh
+++ src/hextopal.sh
@@ -1,1 +1,18 @@
 #!/bin/bash
+
+declare -A DA RE GR YE BL MA CY LI
+
+MA[COLOR]='magenta'
+MA[R]='yes'
+
+printf '%s\n' "${MA[$1]}"
+
+#block-info () {
+#  BLOCK=("${BLA[$2]}" "${RED[$2]}" "${GRE[$2]}")
+#  printf "${BLOCK[$1]}"
+#  if [ "$3" = '1' ]; then
+#    printf '\n'
+#  fi
+#}
+#
+#block-info $1 $2 $3

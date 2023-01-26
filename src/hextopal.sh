e50ea64afbb1c36c90d25dc198dcf65ee7f6ee5b
Jordanyay <99468612+Jordanyay@users.noreply.github.com>
2023-01-26 23:06:40 +1300

    Update files via upload
    Add "rgb-set" function.
    Optimise "hue-shift" function.
    Clean up "hue-shift" function.
    Modify the (loose) naming convention for certain variables.

--- src/hextopal.sh
+++ src/hextopal.sh
@@ -3,66 +3,49 @@
 INPUT=$1
 BASE_16_HEX=($(seq 0 9) a b c d e f)
 RGB_LOOP=(0 1 2)
-HUE_LOOP=(0 1 2 3 4 5)
+HUE_LOOP=(0 60 120 180 240 300)
 declare -A BLA RED GRE YEL BLU MAG CYA WHI
+
+rgb-set() {
+  export R$1=$2 G$1=$3 B$1=$4 
+}
 
 main() {
   for RGB_LOOP_INDEX in "${RGB_LOOP[@]}"; do
     INDEX=0
     case "$RGB_LOOP_INDEX" in
-      0) RGB=R IN_0=${INPUT:0:1} IN_1=${INPUT:1:1} ;;
-      1) RGB=G IN_0=${INPUT:2:1} IN_1=${INPUT:3:1} ;;
-      2) RGB=B IN_0=${INPUT:4:1} IN_1=${INPUT:5:1} ;;
+      0) RGB=R0 IN0=${INPUT:0:1} IN1=${INPUT:1:1} ;;
+      1) RGB=G0 IN0=${INPUT:2:1} IN1=${INPUT:3:1} ;;
+      2) RGB=B0 IN0=${INPUT:4:1} IN1=${INPUT:5:1} ;;
     esac
     for HEX_INDEX in "${BASE_16_HEX[@]}"; do
-      if [ "$HEX_INDEX" = "$IN_0" ]; then
-        OUT_0=$(($INDEX*16))
+      if [ "$HEX_INDEX" = "$IN0" ]; then
+        OUT0=$(($INDEX*16))
       fi
-      if [ "$HEX_INDEX" = "$IN_1" ]; then
-        OUT_1=$INDEX
+      if [ "$HEX_INDEX" = "$IN1" ]; then
+        OUT1=$INDEX
       fi
       ((INDEX++))
     done
-    export $RGB=$(($OUT_0+$OUT_1))
+    export $RGB=$(($OUT0+$OUT1))
   done
-  printf '%b\n' "\e[48;2;$R;$G;${B}m $INPUT \n $R,$G,$B \e[0m"
-  printf '\n'
+  #printf '%b\n' "\e[48;2;$R0;$G0;${B0}m $INPUT \n $R0,$G0,$B0 \e[0m"
+  #printf '\n'
 }
 
 hue-shift() {
-  SHIFT=0
-  for NICE_WORD in "${HUE_LOOP[@]}"; do
-    case "$SHIFT" in
-      0  ) RN=$R GN=$G BN=$B ;;
-      60 ) RN=$R GN=$R BN=$B ;;
-      120) RN=$B GN=$R BN=$B ;;
-      180) RN=$B GN=$R BN=$R ;;
-      240) RN=$G GN=$G BN=$R ;;
-      300) RN=$R GN=$G BN=$R ;;
-      360) RN=$R GN=$G BN=$B ;;
+  for HUE_SHIFT in "${HUE_LOOP[@]}"; do
+    case "$HUE_SHIFT" in
+      0  ) rgb-set 1 $R0 $G0 $B0 ;; # red
+      60 ) rgb-set 1 $R0 $R0 $B0 ;; # yellow
+      120) rgb-set 1 $B0 $R0 $B0 ;; # green
+      180) rgb-set 1 $B0 $R0 $R0 ;; # cyan
+      240) rgb-set 1 $G0 $G0 $R0 ;; # blue
+      300) rgb-set 1 $R0 $G0 $R0 ;; # magenta
     esac
-    if [ "$RN" -gt '255' ]; then
-      RN=255
-    fi
-    if [ "$RN" -lt '0' ]; then
-      RN=0
-    fi
-    if [ "$GN" -gt '255' ]; then
-      GN=255
-    fi
-    if [ "$GN" -lt '0' ]; then
-      GN=0
-    fi
-    if [ "$BN" -gt '255' ]; then
-      BN=255
-    fi
-    if [ "$BN" -lt '0' ]; then
-      BN=0
-    fi
-    printf '%b\n' "\e[48;2;$RN;$GN;${BN}m $RN,$GN,$BN \e[0m"
-    ((SHIFT=SHIFT+60))
+    printf '%b\n' "\e[48;2;$R1;$G1;${B1}m $R1,$G1,$B1 \e[0m"
   done
 }
 
 main
-hue-shift $2
+hue-shift

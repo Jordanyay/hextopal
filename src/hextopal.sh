1a4ac214150c73455acad4e59a9cb73e9cf21b92
Jordanyay <99468612+Jordanyay@users.noreply.github.com>
2023-02-07 23:36:30 +1300

    Update files via upload
    Add "object" associative arrays.
    Add "grab" function (required to justify the "object" implementation).
    Simplify certain elements of the code.
    Remove for loop arrays and "INPUT" variable.
    Remove "color element variables" in favour of "object" associative arrays.

--- src/hextopal.sh
+++ src/hextopal.sh
@@ -1,26 +1,30 @@
 #!/bin/bash
 
-#############
-# Variables #
-#############
-
-INPUT=$1
-BASE_16_HEX=($(seq 0 9) a b c d e f)
-RGB_LOOP=(0 1 2)
-HUE_LOOP=(0 60 120 180 240 300)
+grab() {
+  case "$1" in
+    DAR) printf '%s\n' "${DAR[$2]}" ;;
+    RED) printf '%s\n' "${RED[$2]}" ;;
+    YEL) printf '%s\n' "${YEL[$2]}" ;;
+    GRE) printf '%s\n' "${GRE[$2]}" ;;
+    CYA) printf '%s\n' "${CYA[$2]}" ;;
+    BLU) printf '%s\n' "${BLU[$2]}" ;;
+    MAG) printf '%s\n' "${MAG[$2]}" ;;
+    LIG) printf '%s\n' "${LIG[$2]}" ;;
+  esac
+}
 
 ######################
 # Initial HEX to RGB #
 ######################
 
-for RGB_LOOP_INDEX in "${RGB_LOOP[@]}"; do
+for RGB_LOOP_INDEX in {0..2}; do
   INDEX=0
   case "$RGB_LOOP_INDEX" in
-    0) RGB=R IN0=${INPUT:0:1} IN1=${INPUT:1:1} ;;
-    1) RGB=G IN0=${INPUT:2:1} IN1=${INPUT:3:1} ;;
-    2) RGB=B IN0=${INPUT:4:1} IN1=${INPUT:5:1} ;;
+    0) RGB=R IN0=${1:0:1} IN1=${1:1:1} ;;
+    1) RGB=G IN0=${1:2:1} IN1=${1:3:1} ;;
+    2) RGB=B IN0=${1:4:1} IN1=${1:5:1} ;;
   esac
-  for HEX_INDEX in "${BASE_16_HEX[@]}"; do
+  for HEX_INDEX in {0..9} {a..f}; do
     if [ "$HEX_INDEX" = "$IN0" ]; then
       OUT0=$(($INDEX*16))
     fi
@@ -36,14 +40,14 @@ done
 # Base hue shifts #
 ###################
 
-for HUE_SHIFT in "${HUE_LOOP[@]}"; do
+for HUE_SHIFT in {0..300..60}; do
   case "$HUE_SHIFT" in
-    0  ) RN=Red     RH=000000 RR=$R RG=$G RB=$B ;;
-    60 ) YN=Yellow  YH=000000 YR=$R YG=$R YB=$B ;;
-    120) GN=Green   GH=000000 GR=$B GG=$R GB=$B ;;
-    180) CN=Cyan    CH=000000 CR=$B CG=$R CB=$R ;;
-    240) BN=Blue    BH=000000 BR=$G BG=$G BB=$R ;;
-    300) MN=Magenta MH=000000 MR=$R MG=$G MB=$R ;;
+    0  ) declare -A RED=([NAME]=Red     [HEX]=000000 [RGB]="$R $G $B" [R]=$R [G]=$G [B]=$B) ;;
+    60 ) declare -A YEL=([NAME]=Yellow  [HEX]=000000 [RGB]="$R $R $B" [R]=$R [G]=$R [B]=$B) ;;
+    120) declare -A GRE=([NAME]=Green   [HEX]=000000 [RGB]="$B $R $B" [R]=$B [G]=$R [B]=$B) ;;
+    180) declare -A CYA=([NAME]=Cyan    [HEX]=000000 [RGB]="$B $R $R" [R]=$B [G]=$R [B]=$R) ;;
+    240) declare -A BLU=([NAME]=Blue    [HEX]=000000 [RGB]="$G $G $R" [R]=$G [G]=$G [B]=$R) ;;
+    300) declare -A MAG=([NAME]=Magenta [HEX]=000000 [RGB]="$R $G $R" [R]=$R [G]=$G [B]=$R) ;;
   esac
 done
 
@@ -51,9 +55,9 @@ done
 # Initial RGB to B&W #
 ######################
 
-GREYR=$(($RR*1000))
-GREYG=$(($RG*1000))
-GREYB=$(($RB*1000))
+GREYR=$(($(grab RED R)*1000))
+GREYG=$(($(grab RED G)*1000))
+GREYB=$(($(grab RED B)*1000))
 
 GREYR=$(($GREYR*299))
 GREYG=$(($GREYG*587))
@@ -115,7 +119,7 @@ esac
 
 GREY_SUM=$(($GREYR+$GREYG+$GREYB))
 
-printf '%s\n' "$RR $RG $RB"
+printf '%s\n' "$(grab RED RGB)"
 printf '%s\n' "$GREYR $GREYG $GREYB"
 printf '%s\n' "$(($GREYR+$GREYG+$GREYB))"
 
@@ -129,12 +133,12 @@ print-blank() {
   blank() {
     printf "\e[48;2;$1;$2;${3}m$WIDTH\e[0m"
   }
-  blank $RR $RG $RB
-  blank $YR $YG $YB
-  blank $GR $GG $GB
-  blank $CR $CG $CB
-  blank $BR $BG $BB
-  blank $MR $MG $MB
+  blank $(grab RED RGB)
+  blank $(grab YEL RGB)
+  blank $(grab GRE RGB)
+  blank $(grab CYA RGB)
+  blank $(grab BLU RGB)
+  blank $(grab MAG RGB)
   printf '\n'
   blank $GREY_SUM $GREY_SUM $GREY_SUM
   printf '\n'

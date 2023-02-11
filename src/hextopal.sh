54d81438d15eec03ff3b9801d21609099a6c07ac
Jordanyay <99468612+Jordanyay@users.noreply.github.com>
2023-02-12 00:03:20 +1300

    Update files via upload
    Complete the "greyscale strip" implementation.
    Modify a lot of other aspects
    Reorder some sections.
    Still messy, but tidier than last commit.

--- src/hextopal.sh
+++ src/hextopal.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
 
-prop-get() {
-  case "$1" in
-    BLA) printf "${BLA[$2]}" ;;
-    RED) printf "${RED[$2]}" ;;
-    YEL) printf "${YEL[$2]}" ;;
-    GRE) printf "${GRE[$2]}" ;;
-    CYA) printf "${CYA[$2]}" ;;
-    BLU) printf "${BLU[$2]}" ;;
-    MAG) printf "${MAG[$2]}" ;;
-    WHI) printf "${WHI[$2]}" ;;
-  esac
-}
+H=000000
 
 ######################
 # Initial HEX to RGB #
@@ -36,82 +25,32 @@ for i in {0..2}; do
   export $RGB=$(($OUT0+$OUT1))
 done
 
-###################
-# Base hue shifts #
-###################
+###############
+# Base colors #
+###############
 
-for i in {0..300..60}; do
-  case "$i" in
-    0  ) declare -A RED=([NAME]=Red     [HEX]=$H [RGB]="$R $G $B" [R]=$R [G]=$G [B]=$B) ;;
-    60 ) declare -A YEL=([NAME]=Yellow  [HEX]=$H [RGB]="$R $R $B" [R]=$R [G]=$R [B]=$B) ;;
-    120) declare -A GRE=([NAME]=Green   [HEX]=$H [RGB]="$B $R $B" [R]=$B [G]=$R [B]=$B) ;;
-    180) declare -A CYA=([NAME]=Cyan    [HEX]=$H [RGB]="$B $R $R" [R]=$B [G]=$R [B]=$R) ;;
-    240) declare -A BLU=([NAME]=Blue    [HEX]=$H [RGB]="$G $G $R" [R]=$G [G]=$G [B]=$R) ;;
-    300) declare -A MAG=([NAME]=Magenta [HEX]=$H [RGB]="$R $G $R" [R]=$R [G]=$G [B]=$R) ;;
-  esac
-done
-
-######################
-# Initial RGB to B&W #
-######################
-
-GREYR=$(($(prop-get RED R)*1000))
-GREYG=$(($(prop-get RED G)*1000))
-GREYB=$(($(prop-get RED B)*1000))
-
-GREYR=$(($GREYR*299))
-GREYG=$(($GREYG*587))
-GREYB=$(($GREYB*114))
-
-for i in $GREYR $GREYG $GREYB; do
-  case $i in
-    $GREYR) foo=GREYR ;;
-    $GREYG) foo=GREYG ;;
-    $GREYB) foo=GREYB ;;
-  esac
-  case "$(printf $i | wc -m)" in
-    7) if [ "${i:1:1}" -ge '5' ]; then
-         export $foo=$(printf "$i" | cut -b 1)
-         export $foo=$(($foo+1))
-       else
-         export $foo=$(printf "$i" | cut -b 1)
-       fi ;;
-    8) if [ "${i:2:1}" -ge '5' ]; then
-         export $foo=$(printf "$i" | cut -b 1-2) 
-         export $foo=$(($foo+1))
-       else
-         export $foo=$(printf "$i" | cut -b 1-2)
-       fi ;;
-    9) if [ "${i:3:1}" -ge '5' ]; then
-         export $foo=$(printf "$i" | cut -b 1-3)
-         export $foo=$(($foo+1))
-       else
-         export $foo=$(printf "$i" | cut -b 1-3)
-       fi ;;
-  esac
-done
-
-if [ "$GREYR" -ge "$GREYG" ] && [ "$GREYR" -ge "$GREYB" ]; then
-  GREY=$GREYR
-elif [ "$GREYG" -ge "$GREYB" ]; then
-  GREY=$GREYG
-else
-  GREY=$GREYB
-fi
-
-black-calculator() {
-  local MAX=$(printf '%s\n' "$(prop-get RED R)" "$(prop-get RED G)" "$(prop-get RED B)" | sort -n | tail -n 1)
-  local MIN=$(printf '%s\n' "$(prop-get RED R)" "$(prop-get RED G)" "$(prop-get RED B)" | sort -n | head -n 1)
-  printf '%s\n' "$((($MAX*$MIN)/1000))"
+color() {
+  export $1_N=$2
+  export $1_H=$3
+  export $1_R=$4
+  export $1_G=$5
+  export $1_B=$6
+  export $1_RGB="$4 $5 $6"
 }
 
-white-calculator() {
-  local MAX=$(printf '%s\n' "$(prop-get RED R)" "$(prop-get RED G)" "$(prop-get RED B)" | sort -n | tail -n 1)
-  printf '%s\n' "$MAX"
-}
+color RED Red     $H $R $G $B
+color YEL Yellow  $H $R $R $B
+color GRE Green   $H $B $R $B
+color CYA Cyan    $H $B $R $R
+color BLU Blue    $H $G $G $R
+color MAG Magenta $H $R $G $R
 
-DAR=$(black-calculator)
-LIG=$(white-calculator)
+MAX=$(printf '%s\n' "$RED_R" "$RED_G" "$RED_B" | sort -n | tail -n 1)
+MIN=$(printf '%s\n' "$RED_R" "$RED_G" "$RED_B" | sort -n | head -n 1)
+DAR=$((($MAX*$MIN)/1000))
+
+MAX=$(printf '%s\n' "$RED_R" "$RED_G" "$RED_B" | sort -n | tail -n 1)
+LIG=$MAX
 
 if [ "$DAR" -le 0 ]; then
   DAR=0
@@ -120,41 +59,89 @@ if [ "$LIG" -ge 255 ]; then
   LIG=255
 fi
 
-declare -A BLA=([NAME]=Black [HEX]=$H [RGB]="$DAR $DAR $DAR" [R]=$DAR [G]=$DAR [B]=$DAR)
-declare -A WHI=([NAME]=White [HEX]=$H [RGB]="$LIG $LIG $LIG" [R]=$LIG [G]=$LIG [B]=$LIG)
+color BLA Black $H $DAR $DAR $DAR
+color WHI White $H $LIG $LIG $LIG
 
-printf '%s\n' "$(prop-get BLA RGB)"
-printf '%s\n' "$(prop-get RED RGB)"
-printf '%s\n' "$(prop-get YEL RGB)"
-printf '%s\n' "$(prop-get GRE RGB)"
-printf '%s\n' "$(prop-get CYA RGB)"
-printf '%s\n' "$(prop-get BLU RGB)"
-printf '%s\n' "$(prop-get MAG RGB)"
-printf '%s\n' "$(prop-get WHI RGB)"
-#printf '%s\n' "$GREYR $GREYG $GREYB"
+############################
+# Base colors to greyscale #
+############################
 
-##################
-# Block printing #
-##################
+for WASHED in RED_WAS YEL_WAS GRE_WAS CYA_WAS BLU_WAS MAG_WAS; do
+  R=$((($(printf "$WASHED" | head -c 3)_R*1000)*299))
+  G=$((($(printf "$WASHED" | head -c 3)_G*1000)*587))
+  B=$((($(printf "$WASHED" | head -c 3)_B*1000)*114))
+
+  i=$(($(($R+$G+$B))/3))
+
+  case "$(printf $i | wc -m)" in
+    7) if [ "${i:1:1}" -ge '5' ]; then
+         W=$(($(printf "$i" | cut -b 1)+1))
+       else
+         W=$(printf "$i" | cut -b 1)
+       fi ;;
+    8) if [ "${i:2:1}" -ge '5' ]; then
+         W=$(($(printf "$i" | cut -b 1-2)+1))
+       else
+         W=$(printf "$i" | cut -b 1-2)
+       fi ;;
+    9) if [ "${i:3:1}" -ge '5' ]; then
+         W=$(($(printf "$i" | cut -b 1-3)+1))
+       else
+         W=$(printf "$i" | cut -b 1-3)
+       fi ;;
+  esac
+  export $WASHED="$W $W $W"
+done
+
+############
+# Printing #
+############
+
+printf '\n'
+printf '%s\n' "$BLA_RGB"
+printf '%s\n' "$RED_RGB"
+printf '%s\n' "$YEL_RGB"
+printf '%s\n' "$GRE_RGB"
+printf '%s\n' "$CYA_RGB"
+printf '%s\n' "$BLU_RGB"
+printf '%s\n' "$MAG_RGB"
+printf '%s\n' "$WHI_RGB"
+printf '\n'
+printf '%s\n' "$BLA_RGB"
+printf '%s\n' "$RED_WAS"
+printf '%s\n' "$YEL_WAS"
+printf '%s\n' "$GRE_WAS"
+printf '%s\n' "$CYA_WAS"
+printf '%s\n' "$BLU_WAS"
+printf '%s\n' "$MAG_WAS"
+printf '%s\n' "$WHI_RGB"
+printf '\n'
+
+##########
+# Blocks #
+##########
 
 WIDTH='          '
 
-print-blank() {
-  blank() {
-    printf "\e[48;2;$1;$2;${3}m$WIDTH\e[0m"
-  }
-  blank $(prop-get BLA RGB)
-  blank $(prop-get RED RGB)
-  blank $(prop-get YEL RGB)
-  blank $(prop-get GRE RGB)
-  blank $(prop-get CYA RGB)
-  blank $(prop-get BLU RGB)
-  blank $(prop-get MAG RGB)
-  blank $(prop-get WHI RGB)
-  printf '\n'
-  blank $(prop-get BLA RGB)
-  blank $GREY $GREY $GREY
-  printf '\n'
+blank() {
+  printf "\e[48;2;$1;$2;${3}m$WIDTH\e[0m"
 }
 
-print-blank
+blank $BLA_RGB
+blank $RED_RGB
+blank $YEL_RGB
+blank $GRE_RGB
+blank $CYA_RGB
+blank $BLU_RGB
+blank $MAG_RGB
+blank $WHI_RGB
+printf '\n'
+blank $BLA_RGB
+blank $RED_WAS
+blank $YEL_WAS
+blank $GRE_WAS
+blank $CYA_WAS
+blank $BLU_WAS
+blank $MAG_WAS
+blank $WHI_RGB
+printf '\n\n'
